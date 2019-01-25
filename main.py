363a8abd22bbadef32a4a3271895169e3e22c429
Lwando92 <lwando@kineticskunk.com>
2019-01-25 12:11:13 +0200

    Added a Form and screen for results

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template, redirect, url_for,request, jsonify, abort,request
 from flask_sqlalchemy import SQLAlchemy
+from form import StudentForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret'
@@ -18,54 +19,74 @@ class Student(db.Model):
 
 
 #curl -i http://127.0.0.1:5000/
-@app.route('/', methods=["GET"])
-def retreive_results():
-  data = Student.query.all()
+# @app.route('/', methods=["GET"])
+# def retreive_results():
 
-  output = []
 
-  for x in data:
-    student_data = {}
-    student_data['id'] = x.id
-    student_data['name'] = x.name
-    student_data['physics'] = x.physics
-    student_data['maths'] = x.maths
-    student_data['chemistry'] = x.chemistry
-    output.append(student_data)
 
-  return jsonify({'student': output})
+  
+#   data = Student.query.all()
+
+#   output = []
+
+#   for x in data:
+#     student_data = {}
+#     student_data['id'] = x.id
+#     student_data['name'] = x.name
+#     student_data['physics'] = x.physics
+#     student_data['maths'] = x.maths
+#     student_data['chemistry'] = x.chemistry
+#     output.append(student_data)
+
+#   return jsonify({'student': output})
 
 #curl -i http://127.0.0.1:5000/results/<intID>
-@app.route('/results/<int:indexId>',methods=["GET"])
-def get_one_student(indexId):
+# @app.route('/results/<int:indexId>',methods=["GET"])
+# def get_one_student(indexId):
+#   student = Student.query.filter_by(id = indexId).first()
 
-  student = Student.query.filter_by(id = indexId).first()
+#   if not student:
+#     return jsonify({'message':'No user found'})
 
-  if not student:
-    return jsonify({'message':'No user found'})
+#   student_data = {}
+#   student_data['id'] = student.id
+#   student_data['name'] = student.name
+#   student_data['physics'] = student.physics
+#   student_data['maths'] = student.maths
+#   student_data['chemistry'] = student.chemistry
 
-  student_data = {}
-  student_data['id'] = student.id
-  student_data['name'] = student.name
-  student_data['physics'] = student.physics
-  student_data['maths'] = student.maths
-  student_data['chemistry'] = student.chemistry
-
-  return jsonify({'student':student_data})
+#   return jsonify({'student':student_data})
 
 #curl -i -H "Content-Type: application/json" -X POST -d "{\"name\":\"Sivu\",\"physics\":30,\"maths\":90,\"chemistry\":10}"" http://127.0.0.1:5000/results
-@app.route('/results', methods=['POST'])
+@app.route('/', methods=['GET','POST'])
 def add_results():
-  data = request.get_json()
+    form = StudentForm()
+    if form.validate_on_submit():
+        student = Student(name=form.name.data, physics=form.physics.data, maths=form.maths.data,chemistry=form.chemistry.data,)
+        db.session.add(student)
+        db.session.commit()
+        # flash('Your account has been created! You are now able to log in', 'success')
+  
+    return render_template('home.html', form=form)
 
-  if not data or not 'name' in data:
-    abort(400)
 
-  new_student = Student(name = data['name'],physics= data['physics'],maths= data['maths'], chemistry=data['chemistry'])
-  db.session.add(new_student)
-  db.session.commit()
 
-  return jsonify({'students':'New Student Created'}), 201
+@app.route('/results', methods=['GET','POST'])
+def results():
+  data = Student.query.all()
+  return render_template('results.html', data = data)
+
+
+  # data = request.get_json()
+
+  # if not data or not 'name' in data:
+  #   abort(400)
+
+  # new_student = Student(name = data['name'],physics= data['physics'],maths= data['maths'], chemistry=data['chemistry'])
+  # db.session.add(new_student)
+  # db.session.commit()
+
+
 
 #curl -i -H "Content-Type: application/json" -X PUT -d "{\"name\":\"Sivu\",\"physics\":10,\"maths\":40,\"chemistry\":30}" http://127.0.0.1:5000/results/<intID>
 @app.route('/results/<int:indexId>', methods=['PUT'])
